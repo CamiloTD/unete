ab20e8a94ab87345adcccbd56a6a007e6beaca1b
CamiloTD <camilotd1999@gmail.com>
2018-10-02 14:12:12 -0500

    Fixed Examples

--- examples/consumer.js
+++ examples/consumer.js
@@ -1,11 +1,11 @@
-let { Connector } = require('unete');
+let { Connector } = require('../index');
 
 (async () => {
-	let math = Connector('http://localhost:5000');
-	// *math* will inherit all methods from the microservice located in that port
-	
+	let math = await Connector('http://localhost:5000');
+    // *math* will inherit all methods from the microservice located in that port
+    
 	console.log(await math.add(2, 5));  // 7
 	console.log(await math.sub(5, 2));  // 3
 	console.log(await math.mul(5, 2)); // 10
-	console.log(await math.div(8/2)); // 4
+	console.log(await math.div(8, 2)); // 4
 })();
