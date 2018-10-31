496c4e7c8496e30c0ddc59bd71c4b069973452da
CamiloTD <camilotd1999@gmail.com>
2018-10-31 13:27:38 -0500

    CLI Created

--- unete.js
+++ unete.js
@@ -2,12 +2,41 @@
 let cli = require('commander');
 let path = require('path');
 let Router = require('./router');
+let Connector = require('./connector');
+let repl = require('repl');
 
 cli.version('0.1.0')
+   .option('-c, --connect <url>', 'Connect to unete service')
    .option('-p, --port <port>', 'Set port')
    .option('-m, --module <filename>', 'Set module to export')
    .parse(process.argv);
-let  port   = cli.port;
+let { port, connect }  = cli;
+
+
+if(connect) {
+    console.log(`Connecting to ${connect}...`);
+
+    (async () => {
+        let client = await Connector(connect);
+
+        const r = repl.start({
+            prompt: 'unete> ',
+            eval: async (cmd, ctx, filename, cb) => {
+                if(cmd.trim() === "exit") process.exit(0);
+
+                try {
+                    let rs = await eval('client.' + cmd);
+
+                    cb(null, rs);
+                } catch (exc) {
+                    cb(exc);
+                }
+            }
+        });
+    })();
+
+    return;
+}
 
 if(!port) throw "PORT_EXPECTED";
 
